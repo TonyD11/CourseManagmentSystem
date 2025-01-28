6d42a884f054bf824fd080a5d6da2ed020c84a6a
TonyD11 <cb015830@students.apiit.lk>
2025-01-29 00:19:39 +0530

    create assment structure

--- Views/AssessmentDashboard.cs
+++ Views/AssessmentDashboard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Org.BouncyCastle.Tls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,9 +20,28 @@ namespace CourseManagmentSystem.Views
             this.username = username;
         }
 
+        public void panelController(object Form)
+        {
+            if (this.panel2.Controls.Count > 0)
+            {
+                this.panel2.Controls.RemoveAt(0);
+            }
+            Form frm = Form as Form;
+            frm.TopLevel = false;
+            frm.Dock = DockStyle.Fill;
+            this.panel2.Controls.Add(frm);
+            this.panel2.Tag = frm;
+            frm.Show();
+        }
+
         private void button3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panelController(new CreateAssessment(username));
         }
     }
 }
---
+++ Views/CreateAssessment.cs
@@ -0,0 +1,35 @@
+﻿using CourseManagmentSystem.Controller;
+using CourseManagmentSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseManagmentSystem.Views
+{
+    public partial class CreateAssessment : Form
+    {
+        public string username;
+        public CreateAssessment(string username)
+        {
+            InitializeComponent();
+            this.username = username;
+        }
+
+        private void CreateAssessment_Load(object sender, EventArgs e)
+        {
+            CourseController coursecontroller = new CourseController();
+            List<Course> courses = coursecontroller.GetCourses(username);
+
+            foreach (Course course in courses)
+            {
+                comboBox1.Items.Add(course.Name);
+            }
+        }
+    }
+}
