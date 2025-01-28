8cfc320d927ac7be9a832899decc727bb8155da8
TonyD11 <cb015830@students.apiit.lk>
2025-01-29 00:54:43 +0530

    create assment done

--- Views/CreateAssessment.Designer.cs
+++ Views/CreateAssessment.Designer.cs
@@ -36,6 +36,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -103,11 +104,22 @@
             this.dateTimePicker1.Size = new System.Drawing.Size(216, 22);
             this.dateTimePicker1.TabIndex = 7;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 31);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Create Assesment";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // CreateAssessment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -135,5 +147,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Views/CreateAssessment.cs
+++ Views/CreateAssessment.cs
@@ -28,8 +28,25 @@ namespace CourseManagmentSystem.Views
 
             foreach (Course course in courses)
             {
-                comboBox1.Items.Add(course.Name);
+                comboBox1.Items.Add(course.Code);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string code = comboBox1.Text;
+            string name = textBox1.Text;
+            string discription = textBox2.Text;
+            DateTime due = dateTimePicker1.Value;
+
+            CourseController coursecontroller = new CourseController();
+            Course course = coursecontroller.GetCourse(code);
+
+            Assessment assessment = new Assessment(course, name, discription, due);
+
+            AssessmentController assessmentController = new AssessmentController();
+            assessmentController.CreateAssesment(assessment);
+
         }
     }
 }
