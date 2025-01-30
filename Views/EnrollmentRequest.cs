5a57c63063259500a52a8d76891a238069185f50
dharshan <cb012635@students.apiit.lk>
2025-01-30 12:58:42 +0530

    Enrollment Design

---
+++ Views/EnrollmentRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseManagmentSystem.Views
+{
+    public partial class EnrollmentRequest : Form
+    {
+        public string username;
+        public EnrollmentRequest(string username)
+        {
+            InitializeComponent();
+            this.username = username;
+        }
+
+        private void EnrollmentRequest_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
