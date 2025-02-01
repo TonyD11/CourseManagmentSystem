49a1bdf6d99dc576758531d6000713e535b5d288
dharshan <cb012635@students.apiit.lk>
2025-02-01 10:55:50 +0530

    Student Enrollment Structure

--- Views/Student.cs
+++ Views/Student.cs
@@ -16,5 +16,24 @@ namespace CourseManagmentSystem.Views
         {
             InitializeComponent();
         }
+
+        public void panelController(object Form)
+        {
+            if (this.panel2.Controls.Count > 0)
+            {
+                this.panel2.Controls.RemoveAt(0);
+            }
+            Form frm = Form as Form;
+            frm.TopLevel = false;
+            frm.Dock = DockStyle.Fill;
+            this.panel2.Controls.Add(frm);
+            this.panel2.Tag = frm;
+            frm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panelController(new Enrollment());
+        }
     }
 }
