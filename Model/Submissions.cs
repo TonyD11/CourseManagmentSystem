0ea8b00fdd66b0f556f18298468c816393342f9b
RashenPerera <rashenperera86@gmail.com>
2025-02-02 13:48:58 +0530

    Submission Model

---
+++ Model/Submissions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagmentSystem.Model
+{
+    internal class Submissions
+    {
+        public Submissions(Assessment assessment, User user, DateTime submissionDate, string filePath, string comments)
+        {
+            Assessment = assessment;
+            User = user;
+            SubmissionDate = submissionDate;
+            FilePath = filePath;
+            Comments = comments;
+        }
+
+        public int Id { get; set; }
+        public virtual Assessment Assessment { get; set; }
+        public virtual User User { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        public string FilePath { get; set; }
+        public string Comments { get; set; }
+        public decimal? Grade { get; set; }
+
+    }
+}
